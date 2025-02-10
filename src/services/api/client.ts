4540e33f7d8a68a5465b6b1855a3f8aa086654cc
Thaman Chand <thaman@aiforsite.com>
2025-02-10 21:43:25 +0200

    polish and clean up

--- src/services/api/client.ts
+++ src/services/api/client.ts
@@ -1,30 +1,24 @@
-import { API_URL } from "../../constants";
+import { API_URL } from '../../constants';
 
 export const HSL_API_KEY = import.meta.env.VITE_HSL_API_KEY;
 
 if (!HSL_API_KEY) {
   throw new Error(
-    "HSL API Key is not defined. Please config HSL api key first"
+    'HSL API Key is missing. Please add VITE_HSL_API_KEY to your .env file. See README.md for setup instructions.'
   );
 }
 
-export const fetchGraphQL = async <T>(
-  query: string,
-  variables = {}
-): Promise<T> => {
-  const response = await fetch(
-    `${API_URL}?digitransit-subscription-key=${HSL_API_KEY}`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        query,
-        variables,
-      }),
-    }
-  );
+export const fetchGraphQL = async <T>(query: string, variables = {}): Promise<T> => {
+  const response = await fetch(`${API_URL}?digitransit-subscription-key=${HSL_API_KEY}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
 
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
